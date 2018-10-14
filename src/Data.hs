6c4749b4abbfb787375f93ca5572d50222fbd210
darlliu <darlliu@lavabit.com>
2018-10-13 17:55:26 -0700

    wip

--- src/Data.hs
+++ src/Data.hs
@@ -3,40 +3,94 @@ module Data
     DatasetC(..),
     Row, 
     DatasetR(..),
-    (!?),
-    (!),
-    someFunc
+    (<?>),
+    (<!!>),
+    (<!>)
     ) where
 
 import qualified Data.List as DL
 
 data Column = ICol [Int] | FCol [Float] | SCol [String] 
-    deriving (Show)
+    deriving (Show,Eq)
+
 data DatasetC = DatasetC{
     headerC :: [String],
     dC :: [Column]
-} deriving (Show)
+} deriving (Show, Eq)
 
 type Row = [Float]
 data DatasetR = DatasetR{
     headerR :: [String],
     dR :: [Row]
-} deriving (Show)
+} deriving (Show,Eq)
 
 (!) :: [String] -> String -> Maybe Int
 hh ! h = h `DL.elemIndex` hh
 
-(!?) :: DatasetC -> String -> Maybe Column
-dd !? h = 
+(<?>) :: DatasetC -> String -> Maybe Column  --getCol
+dd <?> h = 
         let hh = headerC dd 
         in do 
         idx <- hh ! h
         return ((dC dd) !! idx)
 
-(<!?>) :: DatasetR -> [String] -> [Row]
+getIdxs :: [String] -> [String] -> Maybe [Int]
+getIdxs hh hs  = let idxs_ = [hh ! x |x<-hs] 
+    in foldr (\x y -> do
+        x_ <- x
+        y_ <- y
+        Just(x_ : y_)) 
+        (Just []) idxs_
+    --   case x of 
+    --     Nothing -> Nothing
+    --     Just x_ -> case y of 
+    --         Nothing -> Nothing
+    --         Just y_ -> Just(x_++[y_])) (Just []) idxs_
 
-(>>) :: DatasetC -> [String] -> DatasetR
+(<!>) :: DatasetC -> [String] -> Maybe DatasetC --subSet Columns
+dc <!> hs = do
+    idxs <- getIdxs (headerC dc) hs
+    let cs = dC dc
+    let cc = fmap (\x -> cs!!x) idxs
+    return DatasetC {
+        headerC = hs,
+        dC = cc
+    }
 
-(++) :: DatasetR -> Column -> DatasetR
+(<!!>) :: DatasetR -> [String] -> Maybe DatasetR --subSet Rows
+dr <!!> hs = do
+    idxs <- getIdxs (headerR dr) hs
+    let rs = dR dr
+    let rr = fmap (\r -> 
+             fmap (\x ->r!!x) idxs) rs
+    return DatasetR {
+        headerR = hs,
+        dR = rr
+    }
 
-join :: DatasetR -> [String] -> [Row] -> DatasetR
+-- cToR :: [Col] :: [Row]
+-- cToR [] = []
+-- cToR col:cols = 
+
+
+-- (toR):: DatasetC :: DatasetR
+-- toR dc = DatasetR{
+--     headerR = headerC dc,
+--     dR = 
+-- }
+
+-- (toC):: DatasetC :: DatasetR
+
+--(>>) :: DatasetC -> [String] -> DatasetR --subSet and convert
+
+(<+>) :: DatasetC -> DatasetC -> DatasetC --Append
+dc1 <+> dc2 = DatasetC {
+    headerC = (headerC dc1) ++ (headerC dc2),
+    dC = (dC dc1) ++ (dC dc2)
+}
+
+(<++>) :: DatasetR -> DatasetR -> DatasetR --Append
+dr1 <++> dr2 = DatasetR {
+    headerR = (headerR dr1) ++ (headerR dr2),
+    dR = zipWith (++) (dR dr1) (dR dr2)
+}
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,34 @@
+import Test.HUnit
 import Data 
 
 ds = DatasetC {
     headerC = ["Int","Float","Obj"],
-    dC = [ICol [1], FCol [0.2], SCol ["Sth"]]
+    dC = [ICol [1,2], FCol [0.1,0.2], SCol ["One","Two"]]
+}
+ds2 = DatasetR {
+    headerR = ["One","Two","Three"],
+    dR = [[1.0, 2.0, 0.0], [1.1, 2.1, 0.1]]
 }
 
-main :: IO ()
-main = do
-    let c = (ds !? "Obj")
-    putStrLn ("\n<<---Testing datasetC get column 1 -->>" ++ (show c))
-    let c = (ds !? "asd")
-    putStrLn ("<<---Testing datasetC get column 2 -->>" ++ (show c))
+testDatasetCGet1 = 
+    TestCase $ assertEqual "Getting a non-empty column"
+      (Just (SCol ["One", "Two"])) (ds <?> "Obj") 
+
+testDatasetCGet2 = 
+    TestCase $ assertEqual "Getting an invalid column"
+      Nothing (ds <?> "Asd")
+
+testDatasetGetIdxs1 = 
+    TestCase $ assertEqual "Valid row indices"
+      (Just DatasetR{
+          headerR = ["One", "Two"],
+          dR = [[1.0, 2.0],[1.1, 2.1]]
+          }) (ds2 <!!> ["One","Two"]) 
+
+testDatasetGetIdxs2 = 
+    TestCase $ assertEqual "Invalid row indices"
+      Nothing (ds2 <!!> ["Asd"]) 
+
+main :: IO Counts
+main = runTestTT $ TestList [testDatasetCGet1, testDatasetCGet2, testDatasetGetIdxs1,
+    testDatasetGetIdxs2]
