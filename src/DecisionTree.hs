9cbe6d931aa41590a9f3b99ca856702053631d3d
darlliu <darlliu@outlook.com>
2018-10-30 16:28:39 -0700

    templates for decision tree

--- src/DecisionTree.hs
+++ src/DecisionTree.hs
@@ -1,2 +1,30 @@
 module DecisionTree where
-    
+import Data
+
+data DTree = DTree { 
+    left :: DTree,
+    right :: DTree,
+    prediction :: Int,
+    feature :: Int,
+    test :: (Int, Double)
+}
+
+maxSplit :: DatasetC -> (Int , Double) 
+maxSplit dc = (0, 1.0)
+
+splitData :: DatasetR -> (Int, Double) -> (DatasetR, DatasetR)
+splitData dr (fidx, thrs) = (dr, dr)
+
+cost :: DatasetC -> Int -> Datum -> Double
+cost dr fidx thrs = 0.0
+
+terminate :: DTree -> Int -> DatasetC -> Bool
+terminate node depth dr = False
+
+trainDT :: DTree -> DatasetR -> Int -> DTree
+trainDT node dr depth =
+    let dc = toC dr in
+    if terminate node depth dc then
+        node
+    else
+        let (dls, drs) = splitData dr (test node) (feature node) in
